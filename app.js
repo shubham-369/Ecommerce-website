c192d7a71d91a2f10ea76dcacf8dcc1c688f4b7c
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-08-01 17:08:15 +0530

    app file added
    app file added with clean up code

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const adminRoutes = require('./routes/admin');
+const userRoutes = require('./routes/shop');
+const contactRoutes = require('./routes/contact');
+const successRoutes = require('./routes/success');
+
+const errorPath = path.join(__dirname, 'views', '404-error.html');
+
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(adminRoutes);
+app.use(userRoutes);
+app.use(contactRoutes);
+app.use(successRoutes);
+
+app.use((req, res, )=>{
+    res.status(404).sendFile(errorPath);
+});
+
+const port =  process.env.port || 4500;
+app.listen(port);
