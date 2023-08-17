b12b983c6d72ebe415a678b59b3fbfa8652d9b6b
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-08-17 14:53:46 +0530

    Delete contact.js

--- routes/contact.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contact = require('../controllers/contact');
-
-router.route('/contact.html').get(contact);
-
-
-module.exports = router;
