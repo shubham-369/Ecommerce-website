38d0070df56ce8d2b25864b35e102a10f15112f7
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-08-17 15:00:08 +0530

    Delete database.js

--- database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node_complete', 'root', 'r333@666m999',{
-    dialect : 'mysql',
-    host : 'localhost'
-});
-
-module.exports = sequelize;
