12139c90792c5519ce5cb5a32c2c4002ffa2276b
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-08-17 14:54:05 +0530

    Delete success.js

--- routes/success.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const success = require('../controllers/success');
-
-router.route('/success.html').get(success)
-
-module.exports = router;
