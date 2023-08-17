170ccd48b223cf14f58a1cf91bff4093c369df4f
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-08-17 14:53:04 +0530

    Delete contact.js

--- controllers/contact.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const contactus = path.join(__dirname,'..' , 'views', 'contact.html');
-
-const contact = async (req, res) =>{
-    await res.sendFile(contactus);
-}
-
-module.exports = contact;
