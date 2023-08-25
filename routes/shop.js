3f6e612ef4a79487fef8ab7839f9814e4e89fae6
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-08-25 18:57:10 +0530

    Delete routes directory

--- routes/shop.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const shopControllers = require('../controllers/shop');
-
-router.get('/', shopControllers.shop);
-router.get('/contact', shopControllers.contact);
-router.get('/success', shopControllers.success);
-
-module.exports = router;
