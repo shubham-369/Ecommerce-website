4231dc6cbfcb7fc1fbee5b20ef4c74a428ce4334
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-08-28 17:40:19 +0530

    Add files via upload

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,9 @@ const express = require('express');
 const adminControllers = require('../controllers/admin');
 const router = express.Router();
 
-router.get('/delete/:deleteID', adminControllers.deleteByID);
 router.get('/admin-product', adminControllers.getProducts);
-router.post('/product', adminControllers.addUpdateProducts);
+router.delete('/admin-product', adminControllers.deleteByID);
+router.post('/add-product', adminControllers.addUpdateProducts);
 router.get('/add-product', adminControllers.fetchByID);
 
 module.exports = router;
