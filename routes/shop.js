28995c92f727e1986deb79a0d656c92cf7358792
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-08-31 16:29:33 +0530

    Add files via upload

--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,21 @@ const router = express.Router();
 const shopControllers = require('../controllers/shop');
 
 router.get('/products', shopControllers.productDetails);
-// router.get('/cart', shopControllers.cart);
+
+router.get('/buy', shopControllers.postOrder);
+
+router.get('/orders', shopControllers.orderPage);
+
+router.get('/cart', shopControllers.getCart);
+
+router.delete('/deleteCart', shopControllers.deleteCart);
+
+router.post('/cart', shopControllers.addCart);
+
 router.get('/contact', shopControllers.contact);
+
 router.get('/success', shopControllers.success);
+
 router.get('/shop', shopControllers.showProducts);
 
 module.exports = router;
