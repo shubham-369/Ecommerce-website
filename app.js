f91eeb3fbd63867be80c3b750858da652fd3a6a8
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-08-31 16:31:30 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const dirname = require('./util/path');
 const sequelize = require('./util/database');
 
@@ -8,15 +9,19 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cartItem');
+const Order = require('./models/order');
+const OrderItem = require('./models/orderItem');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-app.use(express.urlencoded({extended:true}));
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorRoutes = require('./routes/error');
 
+
+app.use(express.json());
+app.use(cors());
 
 app.use(express.static(path.join(dirname, 'views')));
 app.use(express.static(path.join(dirname, 'public')));
@@ -37,10 +42,14 @@ app.use('*', errorRoutes);
 
 Product.belongsTo(User, {constraints:true, onDelete:'CASCADE'});
 User.hasMany(Product);
+
 User.hasOne(Cart);
 Cart.belongsTo(User);
-Product.belongsToMany(Cart, { through:CartItem });
 Cart.belongsToMany(Product, { through:CartItem });
+Product.belongsToMany(Cart, { through:CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 
 const port =  process.env.PORT || 4500;
@@ -51,12 +60,15 @@ sequelize
 })
 .then((user) => {
     if(!user){
-        User.create({name:"name1", email:"test1@gmail.com"});
+        User.create({name:"name3", email:"test3@gmail.com"});
     }else{
         return user;
     }
 })
-.then(() => {
+.then((user) => {
+    return user.createCart();
+})
+.then((cart) => {
     app.listen(port);
 })
 .catch((err) => console.log('Server not running : ',err));
