2bb1a672ebaf27cd7ff6e8e21ec95dbc6351d7d7
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-08-31 16:26:51 +0530

    cart, order update
    here the cart and order system working

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,17 @@
+const Product = require('../models/product');
 const product = require('../models/product');
 
+exports.orderPage = (req, res, next) => {
+    req.user
+    .getOrders({include: ['products']})
+    .then(orders => {
+        res.render('shop/orders', {
+            products: orders,
+            pageTitle: 'Orders',
+            path: '/orders'
+        })
+    })
+}
 
 exports.success = (req, res, next) => {
     res.render('shop/success', {
@@ -46,4 +58,116 @@ exports.productDetails = (req, res, next) => {
             res.status(500).render('error', { pageTitle: 'Internal server error'});
         })
     }
+}
+
+exports.getCart = (req, res, next) => {
+    req.user.getCart()
+    .then((cart) => {
+        return cart
+        .getProducts()
+        .then((products) => {
+            res.render('shop/cart', {
+                prods: products,
+                pageTitle: 'Cart',
+                path: '/cart'
+            });
+        })
+        .catch((error) => {
+            console.log('error while getting products from the cart :', error);
+            res.status(500).render('error', { pageTitle: 'Internal server error'});
+        })
+    })
+    .catch((error) => {
+        console.log('error while fetching product details :', error);
+        res.status(500).render('error', { pageTitle: 'Internal server error'});
+    })
+}
+
+exports.addCart = (req, res, next) => {
+    const {product} = req.body;
+    let fetchedCart;
+    let newQuantity = 1;
+
+    req.user
+    .getCart()
+    .then((cart) => {
+        fetchedCart = cart;
+        return cart.getProducts({where:{id:product}})
+    })
+    .then((products) => {
+        let prod;
+        if(products.length > 0){
+            prod = products[0];
+        }
+        if(prod){
+            const oldQuantity = prod.cartItems.quantity;
+            newQuantity = oldQuantity + 1;
+            return prod
+        }
+        return Product.findByPk(product)
+    })
+    .then((fproduct) => {
+        res.status(200).json({message:'product added successfully to cart'});
+        return fetchedCart.addProduct(fproduct, { 
+            through: { quantity : newQuantity } 
+        });
+    })
+    .catch((error) => {
+        console.log('error while adding product to cart : ', error);
+        res.status(500).json({ message: 'Internal server error'});   
+    })
+}
+
+exports.deleteCart = (req, res, next) => {
+    const delID = req.query.id;
+    req.user
+    .getCart()
+    .then((cart) => {
+        return cart.getProducts({where:{id:delID}});
+    })
+    .then((products) => {
+        const product = products[0];
+        return product.cartItems.destroy();
+    })
+    .then(() => {
+        res.status(200).json({message:'product deleted from the cart'});
+    })
+    .catch((error) => {
+        console.log('error while deleting from cart : ', error);
+        res.status(500).json({ message: 'Internal server error'});        
+    })
+}
+
+exports.postOrder = (req, res, next) => {
+    let fetchedCart;
+    req.user
+    .getCart()
+    .then(cart => {
+        fetchedCart = cart;
+        return cart.getProducts();
+    })
+    .then(products => {
+        return req.user
+            .createOrder()
+            .then(order => {
+                order.addProducts(products.map(product => {
+                    product.orderItems = {quantity: product.cartItems.quantity};
+                    return product;
+                }))
+            })
+            .catch(error => {
+                console.log('error while posting order :', error);
+                res.status(500).json({ message: 'Internal server error'}); 
+            })
+    })    
+    .then(() => {
+        return fetchedCart.setProducts(null);
+    })
+    .then(() => {
+        res.status(200).json({message: 'Ordered successfully!'});
+    })
+    .catch(error => {
+        console.log('error while posting order :', error);
+        res.status(500).json({ message: 'Internal server error'}); 
+    })
 }
